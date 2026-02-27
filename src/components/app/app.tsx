0e16adae7c1868ebe6e814f31dd77d56140b83dc
Makarova Tatiana <masterfriend@yandex.ru>
2026-02-27 12:52:31 +0300

    commit 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,25 @@
 import { AppHeader } from '@components/app-header/app-header';
 import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
 import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
-import { ingredients } from '@utils/ingredients';
 
+//import { ingredients } from '@utils/ingredients';
+
+const ingredients = [
+  {
+    _id: '60666c42cc7b410027a1a9b1',
+    name: 'Краторная булка N-200i',
+    type: 'bun',
+    proteins: 80,
+    fat: 24,
+    carbohydrates: 53,
+    calories: 420,
+    price: 1255,
+    image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+    image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+    image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+    __v: 0,
+  },
+];
 import styles from './app.module.css';
 
 export const App = (): React.JSX.Element => {
